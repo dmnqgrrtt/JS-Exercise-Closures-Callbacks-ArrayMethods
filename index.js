d5983e7f72848d30c3001666780b6ce02493e024
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-02-18 21:15:24 -0500

    finished project, could not figure out stretch

--- index.js
+++ index.js
@@ -194,8 +194,13 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
+function getFullNames(runners) {
   /* CODE HERE */
+  let runnerNames = [];
+  runners.forEach((runner) => {
+    runnerNames.push(`${runner.last_name}, ${runner.first_name}`);
+  });
+  return runnerNames;
 }
 
 /**
@@ -210,8 +215,12 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners) {
   /* CODE HERE */
+  let fNameUpper = runners.map((runner) => {
+    return runner.first_name.toUpperCase();
+  });
+  return fNameUpper;
 }
 
 /**
@@ -227,8 +236,12 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(runners, tShirtSize) {
   /* CODE HERE */
+  let specialSize = runners.filter((runner) => {
+    return runner.shirt_size === tShirtSize;
+  });
+  return specialSize;
 }
 
 /**
@@ -241,8 +254,12 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
+function tallyUpDonations(runners) {
   /* CODE HERE */
+  let totalDonation = runners.reduce((total, runner) => {
+    return total += runner.donation;
+  }, 0);
+  return totalDonation;
 }
 
 /////////////// CLOSURES ///////////////
@@ -263,10 +280,11 @@ function tallyUpDonations(/* CODE HERE */) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
+  let count = 0;
   function counter() {
-    ++count
+    return count++;
   }
+  return counter;
   // BROKEN CODE ENDS
 }
 
@@ -290,8 +308,20 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
+function counterMakerWithLimit(max) {
   /* CODE HERE */
+  let count = 0;
+  function counter() {
+    
+    if(count <= max) {
+      return count++;
+    } else {
+      count = 0;
+      return count++;
+    }
+  }
+  return counter;
+
 }
 
 /////////////// END OF CHALLENGE ///////////////
